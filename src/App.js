84c540f99db7b4075ec733ddffd02e172a5e2a44
rufaid <mohdrufaid7@gmail.com>
2023-12-01 21:36:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import data from './data';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className ="grid-container">
+    <header className="row">
+        <div>
+            <a className="brand" href="index.html">amazona</a>
+        </div>
+        <div>
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+        </div>
+    </header>
+    <main>
+      <div>
+        <div className="row center">
+          {
+            data.products.map(product => (
+              <div key={product._id} className="card">
+              <a href={`/product/${product._id}`}>
+                   {/* image siz:680px by 830px   */}
+                  <img
+                   className="medium"
+                    src={product.image}
+                    alt={product.name} />
+              </a>
+              <div className="card-body">
+              <a href={`/product/${product._id}`}>
+                      <h2>{product.name}</h2>
+                  </a>
+                  <div className="rating">
+                      <span> <i className="fa fa-star"></i> </span>
+                      <span> <i className="fa fa-star"></i> </span>
+                      <span> <i className="fa fa-star"></i> </span>
+                      <span> <i className="fa fa-star"></i> </span>
+                      <span> <i className="fa fa-star"></i> </span>
+                  </div>
+                  <div className="price">
+                     ${product.price}
+                  </div>
+              </div>
+        </div>
+            ))
+          }
+     
+        </div>
+      </div>
+    </main>
+    <footer className="row center">All right reserved  </footer>
+</div>
   );
 }
 
